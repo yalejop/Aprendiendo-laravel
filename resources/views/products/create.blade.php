38f3eefeb6ae693b8d24d26aede30a3d10aae654
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-06-24 11:30:28 -0500

    nombres rutas y vistas de páginas asociadas a las rutas

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -12,11 +12,21 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    Listado de productos
-                    <a class="btn btn-success btn-sm float-right" href="{{ route('products.create') }}">Nuevo Producto</a>
+                    Crear Producto
                 </div>
                 <div class="card-body">
-                    ...
+                    <form action="">
+                    <div class="form-group">
+                        <label for="">Descripción:</label>
+                        <input type="text" class="form-control" name="description">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Precio:</label>
+                        <input type="number" class="form-control" name="price">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                    <a href="{{ route('products.index') }}" class="btn btn-danger">Cancelar</a>
+                    </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('products', function () {
     return view('products.index');
-});
+})->name('products.index');
+
+Route::get('products/create', function () {
+    return view('products.create');
+})->name('products.create');
